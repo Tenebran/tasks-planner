24355be1f6c87310111e885f5a390b1b05b80ba9
Tenebran <sergejgarkusha94@gmail.com>
2026-03-31 12:16:43 +0200

    feat: add ADD-TODOLIST action and extend reducer tests
    - add AddTodolistAT action type
    - extend reducer to handle ADD-TODOLIST
    - update action type union in reducer
    - add test for adding new todolist
    - verify new todolist id and title in state

--- src/reducers/todolists-reducer.test.ts
+++ src/reducers/todolists-reducer.test.ts
@@ -21,3 +21,27 @@ test('correct todolist should be removed', () => {
   expect(endState.length).toBe(1);
   expect(endState[0].id).toBe(todolistId2);
 });
+
+test('correct todolist should be added', () => {
+  //
+  const todolistId1 = 'todolistId1';
+  const todolistId2 = 'todolistId2';
+  const todolistId3 = 'todolistId3';
+
+  const startState: Array<TodoListType> = [
+    { id: todolistId1, title: 'What to learn', filter: 'all' },
+    { id: todolistId2, title: 'What to buy', filter: 'all' },
+  ];
+  //
+
+  const endState = todolistsReducer(startState, {
+    type: 'ADD-TODOLIST',
+    title: 'New-Todo',
+    id: todolistId3,
+  });
+
+  //
+  expect(endState.length).toBe(3);
+  expect(endState[2].title).toBe('New-Todo');
+  expect(endState[2].id).toBe(todolistId3);
+});
--- src/reducers/todolists-reducer.ts
+++ src/reducers/todolists-reducer.ts
@@ -1,17 +1,25 @@
 import type { TodoListType } from '../App';
 
-export type RemoveTodolistAC = {
+export type RemoveTodolistAT = {
   type: 'REMOVE-TODOLIST';
+  id: string;
+};
+
+export type AddTodolistAT = {
+  type: 'ADD-TODOLIST';
+  title: string;
   id: string;
 };
 
 export const todolistsReducer = (
   todolists: TodoListType[],
-  action: RemoveTodolistAC,
+  action: RemoveTodolistAT | AddTodolistAT,
 ): TodoListType[] => {
   switch (action.type) {
     case 'REMOVE-TODOLIST':
       return todolists.filter((t) => t.id !== action.id);
+    case 'ADD-TODOLIST':
+      return [...todolists, { id: action.id, title: action.title, filter: 'all' }];
     default:
       return todolists;
   }
